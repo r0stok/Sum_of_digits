b97f1698d5a43eef0b3a259fd9a762847ec869dc
r0stok <127863180+r0stok@users.noreply.github.com>
2023-04-09 22:08:18 +0300

    Update Sum_of_digits_1.py

--- Sum_of_digits_1.py
+++ Sum_of_digits_1.py
@@ -1,8 +1,23 @@
 def sum_of_digits():
+
     num = input("Input value: ")
     sum=0
 
-    sum = sum(int(i) for i in num)
-print("Output result:", sum)
+    for i in num:
+        sum = sum+int(i)
+
+    print("Output result: ", sum)
 
 sum_of_digits()
+
+#Test numbers:
+#6325678
+#125
+#20
+#568
+#799
+#225566789
+#456655810
+#125879
+#33579
+#1000000
