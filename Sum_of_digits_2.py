884bff1649ccbbc60883e57afeec8641daa16da6
r0stok <127863180+r0stok@users.noreply.github.com>
2023-04-09 22:08:51 +0300

    Update Sum_of_digits_2.py

--- Sum_of_digits_2.py
+++ Sum_of_digits_2.py
@@ -30,3 +30,41 @@ class Student:
             return True
         else:
             return False
+
+# Create instances of the Student class
+john = Student(1, 'John', 'Doe')
+linda = Student(2, 'Linda', 'Jones')
+jason = Student(3, 'Jason', 'Kirk')
+jane = Student(4, 'Jane', 'Doe')
+
+# Add scores for the students
+john.add_score(100)
+john.add_score(95)
+linda.add_score(25)
+linda.add_score(65)
+linda.add_score(80)
+linda.add_score(75)
+jason.add_score(50)
+jason.add_score(60)
+jason.add_score(55)
+jane.add_score(95)
+jane.add_score(80)
+jane.add_score(100)
+
+# Show scores for each student
+john.show_scores()
+linda.show_scores()
+jason.show_scores()
+jane.show_scores()
+
+# Compute and show average score for each student
+print("John's average score:", john.score_average())
+print("Linda's average score:", linda.score_average())
+print("Jason's average score:", jason.score_average())
+print("Jane's average score:", jane.score_average())
+
+# Check if each student passed the course
+print("Did John pass the course?", john.course_passed())
+print("Did Linda pass the course?", linda.course_passed())
+print("Did Jason pass the course?", jason.course_passed())
+print("Did Jane pass the course?", jane.course_passed())
